b95f0200a3b20d884a1aeb85851b122c2fb78cb4
amirhosein mt <amirhoseinmirtalemi@gmail.com>
2023-11-22 21:01:39 +0330

    header components

---
+++ src/Header/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './header.css';
+
+const Header = () => {
+  return (
+    <header className='bg-size-cover bg-position-center py-3'>
+        <div className="container">
+            <h3 className="text-start text-light text-uppercase">
+                fast food catalogue
+            </h3>
+        </div>
+    </header>
+  )
+}
+
+export default Header
